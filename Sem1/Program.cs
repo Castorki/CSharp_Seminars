a01fa35b610789850d61cabfd5d9164cffffbbb4
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-04 21:34:32 +0300

    First commit

---
+++ Sem1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Console.WriteLine("Hello, World!");
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(a*a);
+
+// Console.Write("Input first number; ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input seconde number; ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == num2 * num2)
+// {
+//     System.Console.WriteLine($"{num1} is quad of {num2}");
+// }
+
+// else
+// {
+//     System.Console.WriteLine($"{num1} is not quad of {num2}");
+// }
+
+
+// Console.Write("Input N; ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int start = -n;
+
+// while (start <= n)
+// {
+//     System.Console.Write(start + " ");
+//     start ++;
+// }
+
+Console.Write("Input three-digit number; ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    System.Console.WriteLine($"Sum of {ed} and {sot} -> {ed + sot}");
+}
+else
+System.Console.WriteLine("Yor input not three-digit number");
