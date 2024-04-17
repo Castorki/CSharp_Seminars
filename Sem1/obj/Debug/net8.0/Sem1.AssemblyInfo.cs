11b7e74ce28faba12b152d735f0a61d4d2ea473f
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-17 19:26:28 +0300

    seminar of array

--- Sem1/obj/Debug/net8.0/Sem1.AssemblyInfo.cs
+++ Sem1/obj/Debug/net8.0/Sem1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de122ab6a3594227c744d1e009ac75680c57a565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dffaa01b238e119e4e76daa73cc32a47800272c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
